85f61353786f44201534d274834ef9da8992b0ca
Fabio Berrino <fberrino.pro@gmail.com>
2024-10-28 21:11:33 +0000

    Added password validation and final security

--- YappDown/app/views.py
+++ YappDown/app/views.py
@@ -97,79 +97,82 @@ def note(request, note_id=None):
         error_message = None
 
         user = get_object_or_404(User, user_ID=user_id)
-        notes = Note.objects.filter(user_ID=user)
-        total_storage_used = sum(len(note.content) for note in notes) / (1024 * 1024)  # Convert bytes to MB
-        max_storage = 10  # 10 MB
-        storage_left = max_storage - total_storage_used
+        if user.hasAccess:
+            notes = Note.objects.filter(user_ID=user)
+            total_storage_used = sum(len(note.content) for note in notes) / (1024 * 1024)  # Convert bytes to MB
+            max_storage = 10  # 10 MB
+            storage_left = max_storage - total_storage_used
 
-        if request.method == 'POST':
-            title = request.POST.get('title')
-            content = request.POST.get('content')
-            file = request.FILES.get('file')
-            new_content_size = len(content) / (1024 * 1024)  # Convert bytes to MB
+            if request.method == 'POST':
+                title = request.POST.get('title')
+                content = request.POST.get('content')
+                file = request.FILES.get('file')
+                new_content_size = len(content) / (1024 * 1024)  # Convert bytes to MB
 
-            if file and not title:
-                fs = FileSystemStorage()
-                filename = fs.save(file.name, file)
-                file_url = fs.url(filename)
-                new_content_size += file.size / (1024 * 1024)  # Add file size to content size
+                if file and not title:
+                    fs = FileSystemStorage()
+                    filename = fs.save(file.name, file)
+                    file_url = fs.url(filename)
+                    new_content_size += file.size / (1024 * 1024)  # Add file size to content size
 
-                if file.content_type == 'text/plain':
-                    file_content = file.read().decode('utf-8')
-                    lines = file_content.split('\n')
-                    title = lines[0]
-                    content = '\n'.join(lines[1:])
-                else:
-                    content = ""
-                    title = file.name
+                    if file.content_type == 'text/plain':
+                        file_content = file.read().decode('utf-8')
+                        lines = file_content.split('\n')
+                        title = lines[0]
+                        content = '\n'.join(lines[1:])
+                    else:
+                        content = ""
+                        title = file.name
 
-            if 'delete' in request.POST:
-                if note_id:
-                    note = get_object_or_404(Note, pk=note_id, user_ID_id=user_id)
-                    note.delete()
-                    success_message = "Note removed successfully."
-                    return redirect('home')
-            else:
-                if title and (content or file):
+                if 'delete' in request.POST:
                     if note_id:
                         note = get_object_or_404(Note, pk=note_id, user_ID_id=user_id)
-                        current_note_size = len(note.content) / (1024 * 1024)  # Convert bytes to MB
-                        if Note.objects.filter(title=title, user_ID_id=user_id).exclude(pk=note_id).exists():
-                            error_message = "A note with this title already exists."
-                        elif total_storage_used - current_note_size + new_content_size > max_storage:
-                            error_message = "Not enough storage space to update this note."
-                        else:
-                            note.title = title
-                            note.content = content
-                            if file:
-                                note.file_url = file_url
-                            note.save()
-                            success_message = "Note updated successfully."
-                    else:
-                        if Note.objects.filter(title=title, user_ID_id=user_id).exists():
-                            error_message = "A note with this title already exists."
-                        elif total_storage_used + new_content_size > max_storage:
-                            error_message = "Not enough storage space to add this note."
-                        else:
-                            note = Note.objects.create(title=title, content=content, user_ID_id=user_id)
-                            note.save()
-                            success_message = "Note saved successfully."
+                        note.delete()
+                        success_message = "Note removed successfully."
+                        return redirect('home')
                 else:
-                    error_message = "Title and content are required."
+                    if title and (content or file):
+                        if note_id:
+                            note = get_object_or_404(Note, pk=note_id, user_ID_id=user_id)
+                            current_note_size = len(note.content) / (1024 * 1024)  # Convert bytes to MB
+                            if Note.objects.filter(title=title, user_ID_id=user_id).exclude(pk=note_id).exists():
+                                error_message = "A note with this title already exists."
+                            elif total_storage_used - current_note_size + new_content_size > max_storage:
+                                error_message = "Not enough storage space to update this note."
+                            else:
+                                note.title = title
+                                note.content = content
+                                if file:
+                                    note.file_url = file_url
+                                note.save()
+                                success_message = "Note updated successfully."
+                        else:
+                            if Note.objects.filter(title=title, user_ID_id=user_id).exists():
+                                error_message = "A note with this title already exists."
+                            elif total_storage_used + new_content_size > max_storage:
+                                error_message = "Not enough storage space to add this note."
+                            else:
+                                note = Note.objects.create(title=title, content=content, user_ID_id=user_id)
+                                note.save()
+                                success_message = "Note saved successfully."
+                    else:
+                        error_message = "Title and content are required."
 
-        if note_id:
-            note = get_object_or_404(Note, pk=note_id, user_ID_id=user_id)
+            if note_id:
+                note = get_object_or_404(Note, pk=note_id, user_ID_id=user_id)
+            else:
+                note = None
+
+            notes = Note.objects.filter(user_ID_id=user_id, parent_Note_ID__isnull=True)
+            return render(request, 'note.html', {
+                'note': note,
+                'notes': notes,
+                'success_message': success_message,
+                'error_message': error_message,
+                'storage_left': storage_left
+            })
         else:
-            note = None
-
-        notes = Note.objects.filter(user_ID_id=user_id, parent_Note_ID__isnull=True)
-        return render(request, 'note.html', {
-            'note': note,
-            'notes': notes,
-            'success_message': success_message,
-            'error_message': error_message,
-            'storage_left': storage_left
-        })
+            return redirect('home')
     else:
         return redirect('login')
     
